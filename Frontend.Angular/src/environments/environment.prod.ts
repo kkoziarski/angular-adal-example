76910c5166392a5aca3ca66da135645304bceaba
Krzysztof Koziarski <kkoziarski@objectivity.co.uk>
2017-07-04 11:36:25 +0200

    Frontend - bootstrap installed

--- Frontend.Angular/src/environments/environment.prod.ts
+++ Frontend.Angular/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  envName: 'prod',
+  apiServerUrl: 'http://localhost:44008'
 };
--- Frontend.Angular/src/environments/environment.ts
+++ Frontend.Angular/src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  envName: 'default',
+  apiServerUrl: 'http://localhost:44008'
 };
